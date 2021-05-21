26d244b3966fb64fb491ac117089c536ca3090ed
Chinmaya Biswal <80755486+chinmayabiswal1235@users.noreply.github.com>
2021-05-21 18:44:34 +0530

    DICE-Roll

---
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.diceroller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myButtonListenerMethod();
+    }
+    public void myButtonListenerMethod(){
+        Button button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random rand=new Random();
+                int rollResult=rand.nextInt(6)+1;
+
+                TextView result=(TextView)findViewById(R.id.result);
+                result.setText(Integer.toString(rollResult));
+                ImageView img=(ImageView)findViewById(R.id.dice);
+
+                switch (rollResult){
+                    case 1:
+                    img.setImageResource(R.drawable.dice1);
+                    break;
+                    case 2:
+                        img.setImageResource(R.drawable.dice2);
+                        break;
+                    case 3:
+                        img.setImageResource(R.drawable.dice3);
+                        break;
+                    case 4:
+                        img.setImageResource(R.drawable.dice4);
+                        break;
+                    case 5:
+                        img.setImageResource(R.drawable.dice5);
+                        break;
+                    case 6:
+                        img.setImageResource(R.drawable.dice6);
+                        break;
+
+                }
+            }
+        });
+    }
+}
